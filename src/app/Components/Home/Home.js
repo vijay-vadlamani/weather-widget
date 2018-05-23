2f79122315630bf5afb59fdaba4e4e5f00531b48
Vijay Vadlamani <vijay.vadlamani@grainger.com>
2018-05-22 19:45:08 -0500

    updated location service and removed deprecated api

--- src/app/Components/Home/Home.js
+++ src/app/Components/Home/Home.js
@@ -4,21 +4,13 @@
 import MotherComponent from '../MotherComponent/MotherComponent.js';
 
 import template from './Home.html!text';
-// import stylesheet from './Home.scss!';
+import stylesheet from './Home.css!';
 
 import {geolocation} from '../../Services/geolocation.js';
 import {weather} from '../../Services/weather';
 
 export default class Home extends MotherComponent {
     /**
-     * Creates a full Home component with its whole logic encapsulated:
-     *
-     * * display of the result
-     *
-     * Example:
-     *
-     * ```
-     * var myHome = (new Home('home')).init();
      * ```
      * @namespace Components.Home.Home
      * @class Home
@@ -29,7 +21,40 @@ export default class Home extends MotherComponent {
     constructor(domNode) {
         super(domNode, template);
     }
+    
+    getUserPosition(options) {
+        if (navigator.geolocation) {
+            return new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject, options);
+            })
+        }
+    }
 
+    latAndLngData() {
+        this.getUserPosition()
+        .then((position) => {
+            var latitude = position.coords.latitude,
+                longitude = position.coords.longitude;
+        })
+        .catch((err) => {
+            console.error(err.message);
+        });
+    }
+
+    getWeatherInfo(longitude, latitude) {
+        let geolocationInfo = this.domNode.querySelector('.location')
+        weather(longitude, latitude).then((result) => {
+            let tempInKelvin = result.temp;
+            console.log("result",result);
+            let currentTemp = 1.8*(tempInKelvin - 273) + 32;
+            let currentLocalTemp = Math.round(currentTemp * 10) / 10;
+            let tpl = `
+                Current Temperature : ${currentLocalTemp}
+            `;
+            geolocationInfo.style.display = "block";
+            geolocationInfo.innerHTML = tpl;
+        });
+    }
     /**
      * Inits the Home component, adding all its specific logic.
      * @method init
@@ -37,33 +62,34 @@ export default class Home extends MotherComponent {
      * @return {Components.Home.Home}
      */
     init() {
-
         let geolocationInfo = this.domNode.querySelector('.location');
 
         this.domNode.querySelector('.showLocation').addEventListener('click', () => {
-            geolocationInfo.style.display = "none";
-            geolocation()
-                .then((result) => {
-        let longitude = result.longitude,
-            latitude = result.latitude;
-                    weather(longitude, latitude).then((result) => {
-                        let tempInKelvin = result.temp;
-                        console.log("result",result);
-                let currentTemp = 1.8*(tempInKelvin - 273) + 32;
-                let currentLocalTemp = Math.round(currentTemp * 10) / 10;
-                        let tpl = `
-                            <li>Current Temperature : ${currentLocalTemp} </li>
-                        `;
-                        geolocationInfo.style.display = "block";
-                        geolocationInfo.innerHTML = tpl;
-                    });
+            let locationName = document.getElementsByTagName("input")[0].value;
+            if(locationName == "") {
+                this.getUserPosition().then((res)=> {
+                    console.log(res);
+                    let latitude = res.coords.latitude,
+                        longitude = res.coords.longitude;
+                        this.getWeatherInfo(longitude, latitude);
                 })
-                .catch(e => {
-                    console.error(e);
-                    geolocationInfo.innerHTML = '<li>An error occured</li>';
-                    geolocationInfo.style.display = "block";
-                });
-        }, false);
+            } else {
+                let locationName = document.getElementsByTagName("input")[0].value;
+                geolocation(locationName).then((locationResponse) => {
+                let longitude = locationResponse.results[0].geometry.location.lng,
+                    latitude = locationResponse.results[0].geometry.location.lat;
+                this.getWeatherInfo(longitude, latitude) 
+            })
+            .catch(e => {
+                console.error(e);
+                geolocationInfo.innerHTML = '<li>An error occured</li>';
+                geolocationInfo.style.display = "block";
+            });
+            }
+            
+            geolocationInfo.style.display = "none";
+            
+        });
         return this;
     }
 }
